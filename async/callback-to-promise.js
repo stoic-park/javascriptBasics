534b4d849418c700e6898dacffe8ba53dfcc43ad
stoic-park <demige79@gmail.com>
2020-06-19 14:38:46 +0900

    :fire: Feat : callback-to-promise complete
    - callback-to-promise

---
+++ async/callback-to-promise.js
@@ -0,0 +1,46 @@
+// Callback Hell example
+class UserStorage {
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "ellie" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 2000);
+    });
+  }
+
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "ellie") {
+          resolve({ name: "ellie", role: "admin" });
+        } else {
+          reject(new Error("no access"));
+        }
+      }, 2000);
+    });
+  }
+}
+
+// 1. id, password
+// 2. login
+// 3. Roles
+
+const userStorage = new UserStorage();
+// ! prompt
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`))
+  .cahtch(console.log);
+
+// 콜백 지옥
